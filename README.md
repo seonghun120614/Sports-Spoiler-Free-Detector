6c72c05ebc5d5f75a28668f3c8703c7a4729a6f5
seonghun120614 <seonghun120614@gmail.com>
2026-03-02 22:36:24 +0900

    init setting

---
+++ README.md
@@ -0,0 +1,6 @@
+
+## Python 파일 실행 방법
+
+```python
+uv run main.py
+```
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from sports-spoiler-free-detector!")
+
+
+if __name__ == "__main__":
+    main()
